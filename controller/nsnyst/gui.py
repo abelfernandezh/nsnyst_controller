e84888f1149c9b5b9b36b7b04ddcf569d39c5620
Abel Antonio Fernandez Higuera <abelfernandezh@gmail.com>
2016-01-05 11:49:18 -0500

    Creando la pantalla principal

--- controller/nsnyst/gui.py
+++ controller/nsnyst/gui.py
@@ -1,7 +1,14 @@
-from PyQt4.QtGui import QMainWindow
+from PyQt4.QtGui import QMainWindow, QToolBar
+import qtawesome
 
 
 class MainWindow(QMainWindow):
-
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
+
+        self.showMaximized()
+        self.setWindowTitle('NSNyst Controller')
+
+        self.toolBar = QToolBar()
+        self.toolBar.addAction('Configure Test')
+        self.addToolBar(self.toolBar)
