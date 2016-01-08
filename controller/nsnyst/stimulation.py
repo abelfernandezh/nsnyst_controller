feb4de974c25014751b117490fb8fc80987e4444
Abel Antonio Fernandez Higuera <abelfernandezh@gmail.com>
2016-01-08 12:01:35 -0500

    Creando las clases del módulo de estímulos

---
+++ controller/nsnyst/stimulation.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+
+class Chanel(Enum):
+    Horizontal_Channel = 1
+    Vertical_Channel = 2
+
+
+class Stimuli:
+    def __init__(self, name, duration, channel=0):
+        self.name = name
+        self.chanel = channel
+        self.duration = duration
+
+
+class SaccadicStimuli(Stimuli):
+    def __init__(self, name, duration, amplitude, velocity, channel=0):
+        super(SaccadicStimuli, self).__init__(name, duration, channel)
+        self.amplitude = amplitude
+        self.velocity = velocity
+
+
+class FixationStimuli(Stimuli):
+    def __init__(self, name, duration, amplitude, fixation_duration, variation, channel=0):
+        super(FixationStimuli, self).__init__(name, duration, channel)
+        self.amplitude = amplitude
+        self.fixation_duration = fixation_duration
+        self.variation = variation
