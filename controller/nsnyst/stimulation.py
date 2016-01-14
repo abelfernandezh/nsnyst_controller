c483a84638bad923c39df3ba8acb9460a83caa4b
Abel Antonio Fernandez Higuera <abelfernandezh@gmail.com>
2016-01-14 10:34:28 -0500

    Definiendo una property para que cada estímulo devuelava un diccionario
    Esto será usado para salvar este estímulo a un json de forma fácil

--- controller/nsnyst/stimulation.py
+++ controller/nsnyst/stimulation.py
@@ -21,6 +21,16 @@ class SaccadicStimulus(Stimulus):
         self.amplitude = amplitude
         self.velocity = velocity
 
+    @property
+    def information(self):
+        information = []
+        information.name = self.name
+        information.duration = self.duration
+        information.amplitude = self.amplitude
+        information.velocity = self.velocity
+        information.channel = self.chanel
+        return information
+
 
 class FixationStimulus(Stimulus):
     def __init__(self, name, duration, amplitude, fixation_duration, variation, channel=0):
@@ -28,6 +38,16 @@ class FixationStimulus(Stimulus):
         self.amplitude = amplitude
         self.fixation_duration = fixation_duration
         self.variation = variation
+
+    @property
+    def information(self):
+        information = []
+        information.name = self.name
+        information.duration = self.duration
+        information.amplitude = self.amplitude
+        information.fixation_duration = self.fixation_duration
+        information.variation = self.variation
+        return information
 
 
 class Protocol:
@@ -49,10 +69,14 @@ class Protocol:
         protocol = []
         protocol.name = self.name
         protocol.notes = self.notes
-        protocol.stimuli = array()
+        protocol.saccadic_stimuli = array()
+        protocol.fixation_stimuli = array()
 
         for stimulus in self.stimuli:
-            break
+            if type(stimulus) == SaccadicStimulus:
+                protocol.saccadic_stimuli.append(stimulus)
+            else:
+                protocol.fixation_stimuli.append(stimulus)
 
     def add_stimulus(self, stimulus):
         self.stimuli.append(stimulus)
