17894266efa452656d495cb8d5ba6f26f4d3242e
Abel Antonio Fernandez Higuera <abelfernandezh@gmail.com>
2016-01-14 10:17:25 -0500

    Comenzando con Fidel y Pedro

--- controller/nsnyst/gui.py
+++ controller/nsnyst/gui.py
@@ -1,8 +1,8 @@
 from PyQt4.QtGui import QMainWindow, QToolBar, QDialog, QAction, QFormLayout, QLineEdit, QCheckBox, QSpinBox, QComboBox, \
-    QStackedWidget, QWidget, QFrame, QSizePolicy, QPushButton, QHBoxLayout
+    QStackedWidget, QWidget, QLabel, QPushButton, QHBoxLayout, QTextEdit
 from PyQt4.QtCore import QSize
 import artwork.icons as fa
-from stimulation import Channel
+from stimulation import Channel, Stimulus, SaccadicStimulus
 
 
 class GenericParametersWidget(QWidget):
@@ -97,7 +97,7 @@ class FixationStimuliParametersWidget(QWidget):
 
 class CreateStimuliWidget(QDialog):
     def __init__(self, parent=None):
-        super(QDialog, self).__init__(parent)
+        super(CreateStimuliWidget, self).__init__(parent)
         self.setWindowTitle('Crear nuevo estímulo')
         self.generic = GenericParametersWidget()
         self.fixation_features = FixationStimuliParametersWidget()
@@ -120,7 +120,6 @@ class CreateStimuliWidget(QDialog):
         self.save_button = QPushButton(fa_save, 'Guardar')
         self.save_button.setMaximumWidth(100)
 
-
         self.generic.type.currentIndexChanged.connect(self.on_index_change)
 
     def save_stimuli(self):
@@ -130,6 +129,64 @@ class CreateStimuliWidget(QDialog):
         self.advanced_properties_stack.setCurrentIndex(self.generic.type.currentIndex())
 
 
+class StimulusWidget(QWidget):
+    def __init__(self, stimulus, parent=None):
+        super(StimulusWidget, self).__init__(parent)
+        self.h_layout = QHBoxLayout()
+        self.stimulus_name = QLabel(stimulus.name)
+        self.separator = QLabel('-->')
+        self.stimulus_type = QLabel()
+        if type(stimulus) != SaccadicStimulus:
+            self.stimulus_type.setText('Estímulo Sacádico')
+        else:
+            self.stimulus_type.setText('Estímulo de Fijación')
+        fa_delete = fa.icon('fa.trash')
+        fa_edit = fa.icon('fa.pencil')
+        self.edit_stimulus = QPushButton(fa_edit, '')
+        self.delete_stimulus = QPushButton(fa_delete, '')
+        self.h_layout.addWidget(self.stimulus_name)
+        self.h_layout.addWidget(self.separator)
+        self.h_layout.addWidget(self.stimulus_type)
+        self.h_layout.addWidget(self.edit_stimulus)
+        self.h_layout.addWidget(self.delete_stimulus)
+
+        self.setLayout(self.h_layout)
+
+
+class StimuliList(QWidget):
+    def __init__(self, stimuli_list, parent=None):
+        super(StimuliList, self).__init__(parent)
+
+
+class CreateProtocolWidget(QDialog):
+    def __init__(self, parent=None):
+        super(CreateProtocolWidget, self).__init__(parent)
+        self.setWindowTitle('Crear protocolo')
+
+        self.f_layout = QFormLayout()
+        self.protocol_name = QLineEdit()
+        self.protocol_notes = QTextEdit()
+        self.protocol_notes.setMaximumHeight(50)
+
+        stimulus = SaccadicStimulus('Ejemplo 1', 12, 12, 12, Channel.Horizontal_Channel)
+        self.sti = StimulusWidget(stimulus)
+
+        self.f_layout.addRow('Nombre', self.protocol_name)
+        self.f_layout.addRow('Notas', self.protocol_notes)
+
+        self.f_layout.addRow(self.sti)
+
+        self.setLayout(self.f_layout)
+
+    @property
+    def name(self):
+        return self.protocol_name.text()
+
+    @property
+    def notes(self):
+        return self.protocol_notes.toPlainText()
+
+
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
@@ -137,14 +194,14 @@ class MainWindow(QMainWindow):
         self.showMaximized()
         self.setWindowTitle('NSNyst Controller')
 
-        self.create_stimuli = CreateStimuliWidget()
+        self.create_protocol = CreateProtocolWidget()
 
         self.tool_bar = QToolBar()
         self.tool_bar.setIconSize(QSize(48, 48))
 
         fa_icon = fa.icon('fa.television')
         self.add_stimuli_action = QAction(fa_icon, 'Crear nuevo estímulo', self.tool_bar)
-        self.add_stimuli_action.triggered.connect(self.create_stimuli.show)
+        self.add_stimuli_action.triggered.connect(self.create_protocol.show)
 
         self.tool_bar.addAction(self.add_stimuli_action)
 
--- controller/nsnyst/stimulation.py
+++ controller/nsnyst/stimulation.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from numpy import array
+import json
 
 
 class Channel(Enum):
@@ -6,23 +8,53 @@ class Channel(Enum):
     Vertical_Channel = 2
 
 
-class Stimuli:
+class Stimulus:
     def __init__(self, name, duration, channel=0):
         self.name = name
         self.chanel = channel
         self.duration = duration
 
 
-class SaccadicStimuli(Stimuli):
+class SaccadicStimulus(Stimulus):
     def __init__(self, name, duration, amplitude, velocity, channel=0):
-        super(SaccadicStimuli, self).__init__(name, duration, channel)
+        super(SaccadicStimulus, self).__init__(name, duration, channel)
         self.amplitude = amplitude
         self.velocity = velocity
 
 
-class FixationStimuli(Stimuli):
+class FixationStimulus(Stimulus):
     def __init__(self, name, duration, amplitude, fixation_duration, variation, channel=0):
-        super(FixationStimuli, self).__init__(name, duration, channel)
+        super(FixationStimulus, self).__init__(name, duration, channel)
         self.amplitude = amplitude
         self.fixation_duration = fixation_duration
         self.variation = variation
+
+
+class Protocol:
+    def __init__(self, name, notes):
+        self.protocol_name = name
+        self.protocol_notes = notes
+        self.stimuli = array()
+
+    @property
+    def name(self):
+        return self.protocol_name
+
+    @property
+    def notes(self):
+        return self.protocol_notes
+
+    @property
+    def information(self):
+        protocol = []
+        protocol.name = self.name
+        protocol.notes = self.notes
+        protocol.stimuli = array()
+
+        for stimulus in stimulus
+        return protocol
+
+    def add_stimulus(self, stimulus):
+        self.stimuli.append(stimulus)
+
+    def save(self):
